87fe0c457d61ffe1ce568fa5dea995bfaa8820d0
hornosg <hornosgqgmail.com>
2023-01-24 12:28:30 -0300

    project init

---
+++ src/main/java/com/hornosg/hexagonaldddexample/HexagonalDddExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hornosg.hexagonaldddexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalDddExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalDddExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hornosg/hexagonaldddexample/HexagonalDddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hornosg.hexagonaldddexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalDddExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
